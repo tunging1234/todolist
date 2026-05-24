37563acb5476f3d16541e0489d573d9d27c4f46f
cathy <cathy@gmail.com>
2026-05-24 15:39:45 +0800

    完成刪除

--- todos/urls.py
+++ todos/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from users import views
+
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("todos.urls")),
-    path("users/profile/",views.profile)
+
+    path("",views.todolist,name="todo-list"),
+    path("delete/<int:id>",views.todo_delete,name="todo-delete"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from .models import Todo
 # Create your views here.
+
+def todolist(request):
+    todos = Todo.objects.all()
+    print(todos)
+
+    return render(request,"todos/list.html",{"todos":todos})
+
+def todo_delete(request, id):
+    try:
+        todo = Todo.objects.get(id=id)
+        print(todo)
+        todo.delete()
+
+    except:
+        print("刪除錯誤")
+
+    return redirect("todo-list")
